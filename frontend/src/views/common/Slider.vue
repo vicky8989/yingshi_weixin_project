27519e1ef40bc8f29843a30e2ecbdd7cd81827a5
Princessxia <987875642@qq.com>
2017-11-30 21:04:29 +0800

    添加tab

--- frontend/src/views/common/Slider.vue
+++ frontend/src/views/common/Slider.vue
@@ -28,40 +28,12 @@
 				this.renderSlider()
 			}
 		},
-		methods: {
-			renderSlider() {
-				setTimeout(function() {
-					var slide3 = new auiSlide({
-						container: document.getElementById("aui-slide3"),
-						// "width":300,
-						"height": 300,
-						//               "height":(document.documentElement.clientWidth || document.body.clientWidth)*400/750,
-						"speed": 300,
-						"autoPlay": 3000, //自动播放
-						"pageShow": true,
-						"loop": true,
-						"pageStyle": 'dot',
-						'dotPosition': 'center'
-					});
-				}, 500);
-
-			}
-		},
-		mounted: function() {
-			this.$nextTick(function() {
-
-				// 代码保证 this.$el 在 document 中
-				let _this = this;
-				if(!(_this.list && _this.list.length === 0)) {
-					console.log((document.documentElement.clientWidth || document.body.clientWidth) * 400 / 750)
-					_this.renderSlider();
-				}
-			})
-		},
+		methods: {},
+		mounted() {}
 
 	}
 </script>
-<style type="text/css">
+<style type="text/css" scoped>
 	.bg_cover {
 		width: 100%;
 		height: 300px;
--- frontend/src/views/ranking.vue
+++ frontend/src/views/ranking.vue
@@ -1,53 +1,82 @@
 <template>
 	<div id="ranking">
-		<conutDown :time="endTime"/>
-		<ul class="ranking_list">
-			<li v-for="(list, index) in listData.value" key="index">
-				<div class="ranking_lf">
-					<div class="crown" v-if="index<3"></div>
-					<div class="poho">
-						<img :src="list.pic" />
-					</div>
-				</div>
-				<div class="ranking_center">
-					<h5>{{list.name}}  {{list.number}}号</h5>
-					<!--<span><label for="">票数:</label>{{list.voteNum}}<label for="">礼物:</label>{{list.present}}点</span>-->
-					<span><label for="">票数:</label>{{list.voteNum}}</span>
-					<span>{{list.title}}</span>
-				</div>
-				<div class="ranking_rg" :class="{'ok':index<3}">
-					{{index+1}}
-				</div>
-			</li>
-		</ul>
+		<conutDown :time="endTime" />
+		<mt-navbar v-model="currentSelected">
+			<mt-tab-item id="votes">票数榜</mt-tab-item>
+			<mt-tab-item id="gift">礼物榜</mt-tab-item>
+		</mt-navbar>
+		<mt-tab-container v-model="currentSelected">
+			<mt-tab-container-item id="votes">
+				<ul class="ranking_list">
+					<li v-for="(list, index) in listData.value" key="index">
+						<div class="ranking_lf">
+							<div class="crown" v-if="index<3"></div>
+							<div class="poho">
+								<img :src="list.pic" />
+							</div>
+						</div>
+						<div class="ranking_center">
+							<h5>{{list.name}}  {{list.number}}号</h5>
+							<span><label for="">票数:</label>{{list.voteNum}}<label for="">礼物:</label>{{list.present}}点</span>
+							<span>{{list.title}}</span>
+						</div>
+						<div class="ranking_rg" :class="{'ok':index<3}">
+							{{index+1}}
+						</div>
+					</li>
+				</ul>
+			</mt-tab-container-item>
+			<mt-tab-container-item id="gift">
+				<ul class="ranking_list">
+					<li v-for="(list, index) in listData.value" key="index">
+						<div class="ranking_lf">
+							<div class="crown" v-if="index<3"></div>
+							<div class="poho">
+								<img :src="list.pic" />
+							</div>
+						</div>
+						<div class="ranking_center">
+							<h5>{{list.name}}  {{list.number}}号</h5>
+							<span><label for="">礼物:</label>{{list.present}}点<label for="">票数:</label>{{list.voteNum}}</span>
+							<span>{{list.title}}</span>
+						</div>
+						<div class="ranking_rg" :class="{'ok':index<3}">
+							{{index+1}}
+						</div>
+					</li>
+				</ul>
+			</mt-tab-container-item>
+		</mt-tab-container>
+
 		<BottomNav></BottomNav>
 	</div>
 </template>
 
 <script>
+	import { Navbar, TabItem } from 'mint-ui';
 	import conutDown from './common/conutDown.vue';
 	import BottomNav from './common/BottomNav.vue';
 	import Mock from 'mockjs';
-let maxPageNum = 5;
-// 生成商品列数据
-Mock.mock('userList.json', {
-        code: 1,
-        msg: '查询成功',
-        data: {
-            'total': 20,
-            'records': 10,
-            'page': 1,
-            'rows|10': [{
-                'id|+1': 1,
-                'voteNum': '@natural(10, 100)',
-                'number': '@natural(1, 100)',
-                'name':'@cword(2, 5)',
-                'present':'@natural(1, 100)',
-                'title': '@ctitle(6,20)',
-                'pic': '@image(200x200,#50B347,#fff, nice)'
-            }]
-        }
-});
+	let maxPageNum = 5;
+	// 生成商品列数据
+	Mock.mock('userList.json', {
+		code: 1,
+		msg: '查询成功',
+		data: {
+			'total': 20,
+			'records': 10,
+			'page': 1,
+			'rows|10': [{
+				'id|+1': 1,
+				'voteNum': '@natural(10, 100)',
+				'number': '@natural(1, 100)',
+				'name': '@cword(2, 5)',
+				'present': '@natural(1, 100)',
+				'title': '@ctitle(6,20)',
+				'pic': '@image(200x200,#50B347,#fff, nice)'
+			}]
+		}
+	});
 	export default {
 		data() {
 			return {
@@ -57,7 +86,8 @@ Mock.mock('userList.json', {
 					pageNumber: 1, // 当前显示页号
 					pageSize: 10, // 每页显示数据条数
 					value: [], // 显示数据
-				}
+				},
+				currentSelected: 'votes'
 			}
 		},
 		created() {},
@@ -70,7 +100,7 @@ Mock.mock('userList.json', {
 				this.ApiSever.getRanking(null).then(res => {
 					let result = res.data;
 					if(result.code == 1) {
-						console.log('result.data.rows',result.data.rows);
+						console.log('result.data.rows', result.data.rows);
 						this.listData.value = result.data.rows;
 					}
 				})
