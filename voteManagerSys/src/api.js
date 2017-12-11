4717448ae6cd4d187960933522fdf83df34d75a8
vicky8989 <jiwenchen999@163.com>
2017-12-11 22:25:06 +0800

    add activity list

--- voteManagerSys/src/api.js
+++ voteManagerSys/src/api.js
@@ -12,8 +12,9 @@ const VueHttp = new Vue();
 
 /*测试的接口*/
 const prize = `${HOST}prize`;
+const activeList = `${HOST}getActivity`;
 
-// 生成商品列数据
+// 生成奖品列数据
 Mock.mock(prize, {
   code: 1,
   msg: '查询成功',
@@ -25,15 +26,40 @@ Mock.mock(prize, {
       'id|+1': 1,
       'prizeCount': '@natural(10, 100)',
       'rankname': '@ctitle(6, 20)',
-      'prizename': '@ctitle(6, 20)',      
+      'prizename': '@ctitle(6, 20)',
       'prizeimg': '@image(200x200,#50B347,#fff, nice)'
     }]
   }
 });
 
+// 生成奖品列数据
+Mock.mock(activeList, {
+  code: 1,
+  msg: '查询成功',
+  data: {
+    "success": 'true',
+    "pageSize": 1,
+    "count": 1,
+    "value|2": [{
+      'id|+1': 1,
+      'name': '@ctitle(10, 15)',
+      'reName': '@ctitle(6, 20)',
+      'starttime': "@date('yyyy-MM-dd')",
+      'endtime': "@date('yyyy-MM-dd')",
+      'votestarttime': "@date('yyyy-MM-dd')",
+      'voteendtime': "@date('yyyy-MM-dd')",
+      'status': '1'
+    }]
+  }
+});
+
+
 export default {
   getPrizes:()=>{
     return VueHttp.$http.get(prize)
+  },
+  getActivities:()=>{
+    return VueHttp.$http.get(activeList)
   },
   httpUrl: HOST
 }
--- voteManagerSys/src/views/Index.vue
+++ voteManagerSys/src/views/Index.vue
@@ -1,6 +1,61 @@
 <template>
 	<div>
 		<el-button type="primary" icon="el-icon-plus" @click="handleShow">添加图文投票</el-button>
+    <el-table
+    :data="activityList"
+    border
+    style="width: 1000px; margin-top: 20px;">
+    <el-table-column
+      fixed
+      prop="name"
+      label="活动标题"
+      width="200">
+    </el-table-column>    
+    <el-table-column
+      fixed
+      prop="date"
+      label="报名起止时间"
+      width="125">
+      <template scope="scope">
+          <p>{{scope.row.starttime}}</p>
+          <p>{{scope.row.endtime}}</p>
+      </template>
+    </el-table-column>
+    <el-table-column
+      fixed
+      prop="date"
+      label="投票起止时间"
+      width="125">
+    <template scope="scope">
+          <p>{{scope.row.votestarttime}}</p>
+          <p>{{scope.row.voteendtime}}</p>
+      </template>
+    </el-table-column>
+    <el-table-column
+      fixed
+      prop="date"
+      label="活动状态"
+      width="150">
+      <template scope="scope">
+        <span>报名期</span>/<span>非投票期</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      fixed
+      prop="date"
+      label="操作"
+      width="398">
+      <template scope="scope">
+        <el-button>编辑</el-button>
+        <el-button>投票选项</el-button>
+        <el-button>报名管理</el-button>
+        <el-button>投票记录</el-button>
+        <el-button>删除</el-button>
+        <el-button>活动二维码</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+
 	</div>
 </template>
 
@@ -12,7 +67,7 @@ export default {
   name: 'index',
   data () {
     return {
-
+      activityList:[]
     }
   },
   components: {
@@ -20,9 +75,17 @@ export default {
   methods:{
   	handleShow() {
       this.$router.push('/setting');
+    },
+
+    getActivity(){
+        let self = this;
+        this.ApiSever.getActivities().then(res => {
+            self.activityList = res.data.data.value;
+        });
     }
   },
   mounted() {
+    this.getActivity();
 	}
 }
 </script>
