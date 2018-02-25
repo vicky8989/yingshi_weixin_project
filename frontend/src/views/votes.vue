7c2a34d6bf721b8b680068bcb08ea4ed0af51f00
Princessxia <987875642@qq.com>
2018-02-25 14:57:07 +0800

    展开/收起效果

--- frontend/src/views/votes.vue
+++ frontend/src/views/votes.vue
@@ -24,10 +24,18 @@
 		</ul>
 		<Slider :list="userData.pics" />
 
-		<div class="declaration" @click="open=!open"><i class="iconfont icon_font">&#xe6bf;</i>内容简述：<span v-if="open==true">收起</span><span v-else>展开</span></div>
-		<div v-show="open" class="decs-content">
+		<!--<div class="declaration" @click="open=!open"><i class="iconfont icon_font">&#xe6bf;</i>内容简述：<span v-if="open==true">收起</span><span v-else>展开</span></div>-->
+		<div class="declaration"><i class="iconfont icon_font">&#xe6bf;</i>内容简述：</div>
+		<div class="unfurled_describe" :class="{'unfurled_active':isShow}">
+			{{userData.words}}
+		</div>
+
+		<div  v-if="userData&&userData.words">
+			<span class="unfurled" @click="handleShow">{{showInfo}}</span>
+		</div>
+		<!--<div v-show="open" class="decs-content">
             <slot>{{userData.words}}</slot>
-        </div>
+        </div>-->
 		<div class="enrol">
 			<a href="javascript:;" class="enrol_btn" @click="handleSignin" v-if="isEnroltime">我也要参加</a>
 		</div>
@@ -77,8 +85,8 @@
 			return {
 				userId: this.$route.params.id,
 				openId: this.$route.params.openid,
-				open:false,
-				isUserSendGift:false,
+				open: false,
+				isUserSendGift: false,
 				//isFinished:this.ApiSever.getFinishTime(),
 				userData: {
 					voteNum: 12,
@@ -93,6 +101,8 @@
 					}],
 					gifts: []
 				},
+				isShow: false,
+				showInfo: '展开',
 
 			}
 		},
@@ -140,7 +150,8 @@
 					//请求用户信息
 					self.ApiSever.getPresentsDetail(sid).then(gifts => {
 						//debugger;
-						let giftsInfo = gifts.body,today_ = self.$moment(new Date()).format('YYYY-MM-DD');
+						let giftsInfo = gifts.body,
+							today_ = self.$moment(new Date()).format('YYYY-MM-DD');
 						console.log('gifts', giftsInfo);
 						self.userData.gifts = giftsInfo;
 						self.userData.giftnum = 0;
@@ -153,7 +164,7 @@
 							}
 							self.userData.giftnum += item.num;
 						})
-						self.$store.dispatch('setSendGiftStatus',self.isUserSendGift);
+						self.$store.dispatch('setSendGiftStatus', self.isUserSendGift);
 						self.$forceUpdate();
 					});
 					self.addHot(); //增加热度
@@ -173,8 +184,8 @@
 				//alert(this.$store.state.isSendGiftToday);
 				if(this.$store.state.isSendGiftToday == true) {
 					this_.$toast({
-							message: '您今日已送过礼物了，明日再来！'
-						});
+						message: '您今日已送过礼物了，明日再来！'
+					});
 					return;
 				}
 
@@ -243,29 +254,29 @@
 				//self.$store.commit('voteTime');
 				//if(!self.isVotetime) return false;
 				//当前用户是否今日已投票了
-				self.ApiSever.getVoter(this.$store.state.wxUser.openid,self.userId).then(res => {
+				self.ApiSever.getVoter(this.$store.state.wxUser.openid, self.userId).then(res => {
 					//今日已经投过票了
 					if(res.body.length > 0) {
 						self.$toast({
 							message: '您今日已投过票了！',
-							customClass:'large-font'
+							customClass: 'large-font'
 						});
 					} else {
 						self.addVoter(); //添加今日投票信息
 						self.updateSigner(); //更新得票信息
 					}
-				})			
+				})
 			},
 
 			addVoter() {
 				let voter = {
-					openid:this.$store.state.wxUser.openid,
-					sid:this.userId,
-					time:this.$moment(new Date).format('YYYY-MM-DD')
+					openid: this.$store.state.wxUser.openid,
+					sid: this.userId,
+					time: this.$moment(new Date).format('YYYY-MM-DD')
 				}
-				console.log('votetime',voter.time);
+				console.log('votetime', voter.time);
 				this.ApiSever.addVoter(voter).then(res => {
-					console.log('addVoter',res);
+					console.log('addVoter', res);
 				})
 			},
 			updateSigner() {
@@ -273,7 +284,7 @@
 					this.userData.votenum = 0;
 				this.userData.votenum += 1;
 				this.ApiSever.updateSinger(this.userData._id, this.userData).then(res => {
-					console.log('updateSigner',res);
+					console.log('updateSigner', res);
 				});
 			},
 
@@ -304,7 +315,18 @@
 
 			getTime(time) {
 				return this.$moment(time).format('YYYY-MM-DD hh:mm:ss');
-			}
+			},
+
+			//展开和收起操作
+			handleShow() {
+				this.isShow = !this.isShow;
+				if(this.isShow) {
+					this.showInfo = '收起';
+				} else {
+					this.showInfo = '展开';
+					document.body.scrollTop = 0;
+				}
+			},
 		},
 
 		mounted() {
@@ -315,5 +337,35 @@
 </script>
 
 <style type="text/css" scoped>
-
+	.unfurled_describe {
+		height: 1.5625rem;
+		line-height: 1.5625rem;
+		text-align: left;
+		font-size: 0.75rem;
+		padding: 0 0.6875rem;
+		text-indent: 20px;
+		display: block;
+		overflow: hidden;
+	}
+	.unfurled_active{
+		height: auto;
+		line-height: 1.5625rem;
+		text-align: left;
+		font-size: 0.75rem;
+		padding: 0 0.6875rem;
+		text-indent: 20px;
+		display: block;
+		overflow: hidden;
+	}
+	.unfurled{
+		height: 1.5625rem;
+		line-height: 1.5625rem;
+		text-align: left;
+		font-size: 0.75rem;
+		/*padding: 0.3875rem 0;*/
+		text-align: center;
+		display: block;
+		overflow: hidden;
+		color:#26A2FF;
+	}
 </style>
